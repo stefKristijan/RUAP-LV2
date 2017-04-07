78517b7628ddef1167b0af33bd983a0c331efc2e
stefKristijan <stef.kristijan@gmail.com>
2017-04-07 13:19:54 +0200

    Ex3 - Task 2- Modifying the Index View to Provide a GUI for Creating Contacts

--- ContactManager/Controllers/ContactController.cs
+++ ContactManager/Controllers/ContactController.cs
@@ -9,8 +9,20 @@ using ContactManager.Services;
 
 namespace ContactManager.Controllers
 {
+
+
     public class ContactController : ApiController
     {
+
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
+        }
+
         public Contact[] Get()
         {
             return contactRepository.GetAllContacts();
