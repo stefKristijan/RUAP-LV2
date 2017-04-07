162d4a7b4cb1c925c43b598744c6a91f271e38f9
stefKristijan <stef.kristijan@gmail.com>
2017-04-07 13:07:01 +0200

    Ex2 - Task 1 - Opening the web API project

---
+++ ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using ContactManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ContactManager.Services;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+        
+    private ContactRepository contactRepository;
+
+    public ContactController()
+    {
+        this.contactRepository = new ContactRepository();
+    } 
+    }
+}
